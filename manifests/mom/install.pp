ade8e9cde517a060e0c33e16ecac734139561c2e
Tyghe Vallard <vallardt@gmail.com>
2015-12-11 13:00:49 -0500

    Should support --with-pam so it installs pam module package for torque

--- manifests/mom/install.pp
+++ manifests/mom/install.pp
@@ -5,4 +5,13 @@ class torque::mom::install inherits torque::mom {
         require => Class['torque::build'],
         before  => Class['torque::config'],
     }
+
+    if grep($torque::configure_options, '--with-pam') {
+        exec {"install_torque_pam_${version}":
+            command => "${torque::build_dir}/torque-package-pam-linux-x86_64.sh --install",
+            creates => "/lib64/security/pam_pbssimpleauth.so",
+            require => Class['torque::build'],
+            before  => Class['torque::config'],
+        }
+    }
 }
