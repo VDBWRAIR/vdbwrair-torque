af3ecb70edb6c739699ef84d5c28dd830f079c3c
Tyghe Vallard <vallardt@gmail.com>
2015-12-11 15:33:15 -0500

    server should now rebuild when config options change

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,13 +1,13 @@
 class torque::server::install inherits torque::server {
     exec {"install_torque_server_${torque::version}":
-        command => "${torque::build_dir}/torque-package-server-linux-x86_64.sh --install && touch ${torque::build_dir}/torque_server_installed",
-        creates => "${torque::build_dir}/torque_server_installed",
+        command => "${torque::build_dir}/torque-package-server-linux-x86_64.sh --install && touch ${torque::build_dir}/.torque_server_${torque::version}_${norm_config_options}",
+        creates => "${torque::build_dir}/.torque_server_${torque::version}_${norm_config_options}",
         require => Exec["make_packages_${torque::version}"],
         before  => Class['torque::config'],
     }
     exec {"install_torque_clients_${torque::version}":
-        command => "${torque::build_dir}/torque-package-clients-linux-x86_64.sh --install && touch ${torque::build_dir}/torque_clients_installed",
-        creates => "${torque::build_dir}/torque_clients_installed",
+        command => "${torque::build_dir}/torque-package-clients-linux-x86_64.sh --install && touch ${torque::build_dir}/.torque_clients_${torque::version}_${norm_config_options}",
+        creates => "${torque::build_dir}/.torque_clients_${torque::version}_${norm_config_options}",
         require => Exec["make_packages_${torque::version}"],
         before  => Class['torque::config'],
     }
