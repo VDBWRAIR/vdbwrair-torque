1c4edaa1b3883116c520bfa91d36367df5e0d9b6
Tyghe Vallard <vallardt@gmail.com>
2015-12-11 14:59:14 -0500

    replace [-=\s] with '' for hash

--- manifests/build.pp
+++ manifests/build.pp
@@ -4,7 +4,7 @@ class torque::build inherits torque {
     } else {
         $config_options = ""
     }
-    $normalized_options = regsubst(${config_options, "[-=\s]", "", "G")
+    $normalized_options = regsubst($config_options, "[-=\s]", "", "G")
     $config_sha = pw_hash($normalized_options, 'MD5', $torque::version)
     notify{"${configure_options}":}
     notify{"${config_sha}":}
